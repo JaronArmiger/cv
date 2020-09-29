2bf48f3895572bd80ea5b09daf4611c27992fa62
JaronArmiger <jaron.armiger@gmail.com>
2020-09-29 16:59:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+CV app for odin project
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+  	super();
+  }
+
+  render() {
+  	return (
+      <div>
+      	<h1>u remind me</h1>
+      </div>
+  	);
+  }
+}
+
+export default App;
