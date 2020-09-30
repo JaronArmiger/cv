6b653372e08d831c54605474606e02d4c2e24833
JaronArmiger <jaron.armiger@gmail.com>
2020-09-29 22:13:59 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-CV app for odin project
---
+++ precache-manifest.a812adec31dcd8d4f8bb65282d76f285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df5d24b2195d76734a20a4dd94c04ef",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "3926375ade56759e6d98",
+    "url": "/cv/static/css/main.0f608128.chunk.css"
+  },
+  {
+    "revision": "d0a1a0da9822e7afd60b",
+    "url": "/cv/static/js/2.0b890269.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.0b890269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3926375ade56759e6d98",
+    "url": "/cv/static/js/main.52691347.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  }
+]);
